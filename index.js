970fd9e4e32277b91186245b70f14b03059a5b7d
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-11-08 22:27:58 +0700

    feat: finish UI, add static image

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const postsRouter = require('./routes/api/posts');
+const { resolve, dirname } = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use('/api/posts', postsRouter);
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
