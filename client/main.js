2134c73f0ea22a30afa33682de32f970f86b16c6
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-11-09 07:22:40 +0700

    feat: implement infinite scrolling

--- client/main.js
+++ client/main.js
@@ -1,18 +1,24 @@
 const postsGroup = document.querySelector('.posts-group');
 
-async function main() {
-    const posts = await getPosts();
-    showPosts(posts);
+const limit = 5;
+let offset = 1;
+
+function loadPosts() {
+    setTimeout(async () => {
+        const posts = await getPosts();
+        showPosts(posts);
+        offset += limit;
+    }, 500);
 }
 
-function showLoading() {
-    postsGroup.textContent = 'Loading...';
-}
+// function showLoading() {
+//     postsGroup.textContent = 'Loading...';
+// }
 
 async function getPosts() {
     try {
-        showLoading();
-        const res = await fetch('http://localhost:5000/api/posts');
+        // showLoading();
+        const res = await fetch(`http://localhost:5000/api/posts?limit=${limit}&offset=${offset}`);
         if (res.ok) {
             const { data: posts } = await res.json();
             return posts;
@@ -23,30 +29,43 @@ async function getPosts() {
 }
 
 function showPosts(posts = []) {
-    let postElements = '';
     posts.map(post => {
-        postElements += getPostElement(post);
+        postsGroup.append(getPostElement(post));
     });
-    postsGroup.innerHTML = postElements;
 }
 
 function getPostElement({ image, username, content, like }) {
-    return `
-    <div class="post">
-            <div class="flex ai-c post-info">
-                <div class="post-image">
-                    <img src="http://localhost:5000/images/${image}" alt="">
-                </div>
-                <div>
-                    <a href="#" class="post-username">${username}</a>
-                    <span class="post-like">${like} ${like == 1 ? 'like' : 'likes'}</span>
-                </div>
+    const postDiv = document.createElement('div');
+    postDiv.setAttribute('class', 'post');
+    postDiv.innerHTML = `
+        <div class="flex ai-c post-info">
+            <div class="post-image">
+                <img src="http://localhost:5000/images/${image}" alt="">
             </div>
-            <div class="post-content">
-                <p>${content}</p>
+            <div>
+                <a href="#" class="post-username">${username}</a>
+                <span class="post-like">${like} ${like == 1 ? 'like' : 'likes'}</span>
             </div>
         </div>
-    `;
+        <div class="post-content">
+            <p>${content}</p>
+        </div>`;
+    return postDiv;
 }
+loadPosts();
 
-main();
+window.addEventListener('scroll', throttle(() => {
+    loadPosts();
+}, 1000), {
+    passive: true
+});
+
+function throttle(fn, wait) {
+    let time = Date.now();
+    return function () {
+        if (time + wait - Date.now() < 0) {
+            fn();
+            time = Date.now();
+        }
+    }
+}
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,8 +5,13 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     const { limit = 5, offset = 1 } = req.query;
+    console.log(limit, offset);
     try {
-        const posts = await Post.findAll({ limit: parseInt(limit), offset: parseInt(offset) });
+        const posts = await Post.findAll({
+            limit: parseInt(limit),
+            offset: parseInt(offset),
+            order: [['createdAt', 'DESC']],
+        });
         return res.status(200).json({ data: posts });
     } catch (error) {
         return res.status(500).json({ message: error.message });
